c4397c711ae2794dd8e5a492a0b81ccd75886f1d
shumaneker <55175560+shumaneker@users.noreply.github.com>
2023-07-12 16:32:52 +0530

    changed table name and field names

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 		}
 
 		// Query database for all rows in the table
-		$sql = "SELECT * FROM mytable";
+		$sql = "SELECT * FROM employees";
 		$result = $conn->query($sql);
 
 		if ($result->num_rows > 0) {
@@ -29,7 +29,7 @@
 			echo "<table><tr><th>ID</th><th>Name</th><th>Email</th></tr>";
 			// Loop through results and display each row in the table
 			while($row = $result->fetch_assoc()) {
-				echo "<tr><td>" . $row["id"] . "</td><td>" . $row["name"] . "</td><td>" . $row["email"] . "</td></tr>";
+				echo "<tr><td>" . $row["emp_no"] . "</td><td>" . $row["first_name"] . "</td><td>" . $row["email_id"] . "</td></tr>";
 			}
 			echo "</table>";
 		} else {
