f32e7d9641c500d43edf514e6de7f6c6b91deff6
shumaneker <55175560+shumaneker@users.noreply.github.com>
2023-07-13 10:59:26 +0530

    re-checking if db is getting connected

--- index.php
+++ index.php
@@ -16,6 +16,11 @@
 		// Create database connection
 		$conn = mysqli_connect($servername, $username, $password, $dbname, $dbport);
 
+		
+		if (mysqli_connect_errno()) {
+		  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		  exit();
+		}
 		// Check connection
 		if ($conn->connect_error) {
 			echo "Game Over";
